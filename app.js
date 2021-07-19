6b390e835a2a15082e2c8aa97826443552504974
yuricode-123 <iyoule@naver.com>
2021-07-19 22:16:12 +0900

    js 수정

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ if(isNaN(age) || age < 0) {
   console.log("You are too young.");
 } else if (age > 50 && age <= 80){
   console.log("You should exercise.");
+} else if (age === 100) {
+  console.log("wow you are wise.");
 } else if (age > 80) {
   console.log("you can do whatever you wanst.");
 };
