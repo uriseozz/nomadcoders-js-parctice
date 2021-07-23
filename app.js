e73e6cb5edb6d43794e17db9657da0e667e03153
yuricode-123 <iyoule@naver.com>
2021-07-23 23:01:01 +0900

    시작

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-const age = parseInt(prompt("How old are you?"));
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
 
-console.log();
-
-if(isNaN(age) || age < 0) {
-  console.log("Please write a real positive numvber.")
-} else if (age < 18){
-  console.log("You are too young.");
-} else if (age >= 18 && age <= 50){
-  console.log("You are too young.");
-} else if (age > 50 && age <= 80){
-  console.log("You should exercise.");
-} else if (age === 100) {
-  console.log("wow you are wise.");
-} else if (age > 80) {
-  console.log("you can do whatever you wanst.");
+function onLoginSubmit(event) {
+  event.preventDefault();
+  console.log(event);
 };
+
+loginForm.addEventListener("submit", onLoginSubmit);
